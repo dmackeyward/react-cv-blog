032804cf69e62fae40ac5c0b28b4bd7494aa5d2b
unknown <biggie9925@hotmail.com>
2023-09-14 14:58:44 +1200

    started header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import classes from "./Header.module.css"
+import icon from "../assets/icon.jpg"
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/"><img src={icon}></img></a>
+                        </li>
+                        <li>
+                            <a href="/cv.pdf" target="_blank" rel="noopener noreferrer"><button>Download CV</button></a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+} 
+    
+export default Header
