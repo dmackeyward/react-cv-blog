0a6d79117241fdd447488517765907fcedff5270
unknown <biggie9925@hotmail.com>
2023-09-27 14:37:22 +1300

    updated AboutMe to Aboutme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Header from "./components/Header"
-import AboutMe from "./components/AboutMe"
+import AboutMe from "./components/Aboutme"
 import Skills from "./components/Skills"
 import Footer from "./components/Footer"
 
